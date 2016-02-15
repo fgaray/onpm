1749755a75f68df16167cf2f1cd90d8e47a3ca6e
Felipe Garay <felipe.garay@usach.cl>
2016-02-15 23:36:42 +0100

    ADD: filter opam's switchs

--- src/common.ml
+++ src/common.ml
@@ -1,13 +1,14 @@
 
 let print_array a = 
-    Array.map (fun x -> print_string (x ^ " ")) a;
+    ignore (Array.map (fun x -> print_string (x ^ " ")) a);
     print_string "\n"
 
-let print_list l =
-    List.map (fun x -> print_string (x ^ " ")) l;
+let print_list fn l =
+    ignore (List.map (fun x -> print_string ((fn x) ^ "; ")) l);
     print_string "\n"
 
 let drop i xs = Array.sub xs 1 (Array.length xs - 1)
+
 
 let rec read_all_command channel =
     try
@@ -15,3 +16,36 @@ let rec read_all_command channel =
         str :: read_all_command channel
     with
         End_of_file -> []
+
+
+let rec drop_while fn l =
+    match l with
+    | x :: xs -> if fn x 
+                    then drop_while fn xs
+                    else xs
+    | []      -> []
+
+
+let rec take_while fn l =
+    match l with
+    | x :: xs -> if fn x 
+                    then x :: take_while fn xs
+                    else []
+    | []      -> []
+
+let rec take_while_string fn s =
+    let chr = String.get s 0 in
+    if fn chr
+        then String.make 1 chr ^ take_while_string fn (String.sub s 1 (String.length s - 1))
+        else ""
+
+let rec drop_while_string fn s =
+    let chr = String.get s 0 in
+    if fn chr
+        then drop_while_string fn (String.sub s 1 (String.length s - 1))
+        else s
+
+
+let eq x y = x = y
+
+let neq x y = x <> y
--- src/opam_interface.ml
+++ src/opam_interface.ml
@@ -5,8 +5,11 @@ open Common
 type switch_list =
     {
         name : string;
+        state : string;
         origin : string; 
     }
+
+let switch_list_to_string sl = "{ name = " ^ sl.name ^ "; origin = '" ^ sl.origin ^ "'; state = " ^ sl.state ^ "}"
 
 
 let run_opam_switch_list = 
@@ -15,12 +18,25 @@ let run_opam_switch_list =
 
 
 let parse_output_switch_list xs =
-    let parse_line item = Str.split (Str.regexp " ") item in
+    let parse_line item = 
+        let name = take_while_string (neq ' ') item in
+        let name_droped = drop_while_string (neq ' ') item in
+        let space_droped = drop_while_string (eq ' ') name_droped in
+        let state = take_while_string (neq ' ' ) space_droped in
+        let state_droped = drop_while_string (neq ' ') space_droped in
+        let space_droped = drop_while_string (eq ' ') state_droped in
+        let origin = take_while_string (neq ' ' ) space_droped in
+        { name = name;
+          state = state;
+          origin = origin;
+        }
+    in
     List.map parse_line xs
 
 
 let switch_exists switch =
     let result = run_opam_switch_list in
-    print_list result;
-    List.map print_list (parse_output_switch_list result);
+    let parsed = parse_output_switch_list result in
+    let named = List.filter (fun x -> x.origin = "node_modules") parsed in
+    print_list switch_list_to_string named;
     ()
